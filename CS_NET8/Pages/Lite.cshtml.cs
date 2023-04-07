fffc81a799b47fc59da41ce40e7a8e812e8c3f6d
redsoftware-ctruxaw <ctruxaw@redsoftware.com>
2023-04-07 02:34:16 -0700

    ASP.NET 8.0 sample added

---
+++ CS_NET8/Pages/Lite.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using RadPdf.Lite;
+using RadPdf.Web.UI;
+
+namespace RadPdfNet7Demo.Pages
+{
+    public class LiteModel : PageModel
+    {
+        private IHostingEnvironment _env;
+
+        public LiteModel(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        public void OnGet()
+        {
+            string path = System.IO.Path.Combine(_env.WebRootPath, "pdfs", "RadPdfSampleForm.pdf");
+
+            // Get PDF as byte array from file (or database, browser upload, remote storage, etc)
+            byte[] pdfData = System.IO.File.ReadAllBytes(path);
+
+            // Create RAD PDF control
+            PdfWebControlLite pdfWebControl1 = new PdfWebControlLite(HttpContext);
+
+            // Create document from PDF data
+            pdfWebControl1.CreateDocument("Document Name", pdfData);
+
+            // Put control in ViewBag
+            ViewData["PdfWebControl1"] = pdfWebControl1;
+        }
+    }
+}
