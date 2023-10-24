8dfd72de54d0e6d2d8c5e124ec736c902e05661e
redsoftware-chris <ctruxaw@redsoftware.com>
2023-10-23 17:40:24 -0700

    Updated for RAD PDF version 3.40.0 ASP.NET Blazor sample ICI

---
+++ CS_Blazor/BlazorApp1/CustomPdfIntegrationProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Resources;
+using System.Web;
+
+using RadPdf.Data.Document.Objects.Shapes;
+using RadPdf.Integration;
+
+public class CustomPdfIntegrationProvider : PdfIntegrationProvider
+{
+    public CustomPdfIntegrationProvider() : base()
+    {
+        // Replace this session provide with your own
+        this.LiteSessionProvider = new CustomPdfLiteSessionProvider();
+    }
+}
---
+++ CS_Blazor/BlazorApp1/CustomPdfLiteSessionProvider.cs
@@ -0,0 +1,71 @@
+﻿#define NET40
+
+using System;
+#if NET40
+using System.Collections.Concurrent;
+#else
+using System.Collections.Generic;
+#endif
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using RadPdf.Lite;
+
+// Not in use by default
+// Uncomment the approprate line in CustomPdfIntegrationProvider.cs
+public class CustomPdfLiteSessionProvider : PdfLiteSessionProvider
+{
+    // This example uses an in memory dictionary, which won't have  
+    // persistent storage, but a database or other key /value store 
+    // can easily be substituted. 
+#if NET40
+    private readonly ConcurrentDictionary<string, byte[]> _dict;
+#else
+    private readonly Dictionary<string, byte[]> _dict;
+#endif
+
+    public CustomPdfLiteSessionProvider()
+        : base()
+    {
+#if NET40
+        _dict = new ConcurrentDictionary<string, byte[]>();
+#else
+        _dict = new Dictionary<string, byte[]>();
+#endif
+    }
+
+    public override string AddSession(HttpContext context, PdfLiteSession session)
+    {
+        string key = GenerateKey();
+
+#if !NET40
+        lock (_dict)
+        {
+#endif
+            _dict[key] = session.Serialize();
+#if !NET40
+        }
+#endif
+        return key;
+    }
+
+    public override PdfLiteSession GetSession(HttpContext context, string key)
+    {
+        byte[] data;
+
+#if !NET40
+        lock (_dict)
+        {
+#endif
+            data = _dict[key];
+#if !NET40
+        }
+#endif
+
+        if (null == data)
+        {
+            return null;
+        }
+
+        return PdfLiteSession.Deserialize(data);
+    }
+}
